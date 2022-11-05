7918f1584fccd6eb28001d4ae5d68d4fb43e91ad
lauvsong <hyehye9355@naver.com>
2022-11-05 18:09:04 +0900

    :sparkles: 최소 전략 구현

--- Player1.js
+++ Player1.js
@@ -10,7 +10,19 @@ class Player1 {
      * player_name은 printLog 할때 누구의 로그인지를 구분하기 위해 사용할 수 있고,
      * Score Board에 표시됩니다.
      */
-    player_name = "_will be replaced_";
+    player_name = "lauvsong";
+
+    STRIKE = 0;
+    BALL = 1;
+    ANTA = 2;
+    HOMERUN = 3;
+
+    ball3_turn_cnt = 0;
+    hit = 0;
+
+    is_offence = true;
+    ball4_target = [1,1];
+    prev_ball3 = false;
 
     /**
      * 게임이 시작되면 호출됩니다.
@@ -50,14 +62,20 @@ class Player1 {
      *   공격(타자)인 경우, 스윙하지 않은 것으로 간주합니다.
      */
     onTurnStart(data) {
-        // row: 0 or 1 or 2 or 3 중 랜덤 선택. 3은 범위 밖의 선택.
-        let row = Math.floor(Math.random() * 4);
-        // col: 0 or 1 or 2 or 3 중 랜덤 선택. 3은 범위 밖의 선택.
-        let col = Math.floor(Math.random() * 4);
+        let result;
 
-        printLog(this.player_name+": onTurnStart! ["+row+", "+col+"]");
+        if (data.is_offence) {
+            this.is_offence = true;
+            result = this.attack(data)
+        }
+        else {
+            this.is_offence = false;
+            result = this.defense(data) 
+        }
+
+        printLog(this.player_name+": onTurnStart! " + result);
         
-        return [row, col];
+        return result;
     }
 
     /**
@@ -78,9 +96,20 @@ class Player1 {
      */
     onTurnEnd(result) {
         printLog(this.player_name+": onTurnEnd! choice:"+JSON.stringify(result.choice)+", result:"+result.result);
+        // printLog("strike: "+result.ball_count.s+" ball: "+result.ball_count.b+" out: "+result.ball_count.o)
+        if (this.prev_ball3 && result.result != this.STRIKE) {
+            this.ball3_turn_cnt++
+            this.prev_ball3 = false
+
+            if ((result.result == this.HOMERUN) || (result.result == this.ANTA)) {
+                this.hit++
+            }
+        } else if (result.ball_count.b == 3) {
+            this.prev_ball3 = true
+        }
     }
 
-    /**
+    /*치
      * 라운드가 끝나면 호출되며, 라운드의 결과를 받아봅니다.
      * @param result
      *   result.win: -1(졌을때) or 0(비겼을때) or 1(이겼을때)
@@ -90,6 +119,14 @@ class Player1 {
      */
     onRoundEnd(result) {
         printLog(this.player_name+": onRoundEnd! win:"+result.win+", score:"+JSON.stringify(result.score));
+        let hit_rate = this.hit / this.ball3_turn_cnt;
+        // printLog("hit: "+this.hit+" ballturn: "+this.ball3_turn_cnt)
+        if (!this.is_offence && hit_rate >= 0.5) {
+            this.ball4_target = [2,1];
+        }
+
+        this.hit = 0;
+        this.ball3_turn_cnt = 0;
     }
 
     /**
@@ -100,4 +137,34 @@ class Player1 {
     onGameEnd(result) {
         printLog(this.player_name+": onGameEnd! win:"+result.win);
     }
+
+    // # 공격 전략
+    // 일단 안 치기.
+    // 볼넷 직전 [1,1] 한번 선택해보기.
+    // 실패 시 뒤로 안 치기.
+    attack(data) {
+        if (!this.prev_ball3 && data.ball_count.b == 3) 
+            return [1,1]
+        else 
+            return null
+        
+    }
+
+    // # 수비 전략
+    // 일부러 볼셋, 그 뒤로 계속 [1,1]
+    // - 볼셋까지 가기 전 타자가 헛스윙해서 3스트라이크 할 확률도..
+    defense(data) {
+        if (data.ball_count.b == 3) {
+            return this.ball4_target
+        }
+        else
+            return null
+    }
 }
+
+// 데이터 수집. (볼넷 직전 상대방의 좌표)
+
+// ## 개선 전략
+// 1. 홈런 / 안타 당한 비율 (볼넷은 불가능)
+//     1. 50% 이하 시 유지
+//     2. 그 이상일 시 상대의 전략임. 마지막 볼 1/3 존으로 변경 (1행 제외)
